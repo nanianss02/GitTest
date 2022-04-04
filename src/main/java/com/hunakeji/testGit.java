84202410a72b172cd5150e12801a8c69b14fd96f
nanian <268353@qq.com>
2022-04-04 19:57:38 +0800

    push Commit

--- src/main/java/com/hunakeji/testGit.java
+++ src/main/java/com/hunakeji/testGit.java
@@ -3,5 +3,7 @@ package com.hunakeji;
 public class testGit {
     public static void main(String[] args) {
         System.out.println("我是git中的第一个方法");
+        System.out.println("我修改了本地库中的代码");
+
     }
 }
