4d358937a68a66d7a425302d0e804c18e430e9b5
nanianss02 <102537826+nanianss02@users.noreply.github.com>
2022-04-04 20:17:20 +0800

    commit

--- src/main/java/com/hunakeji/testGit.java
+++ src/main/java/com/hunakeji/testGit.java
@@ -4,6 +4,7 @@ public class testGit {
     public static void main(String[] args) {
         System.out.println("我是git中的第一个方法");
         System.out.println("我修改了本地库中的代码");
+         System.out.println("pull test");
 
     }
 }
